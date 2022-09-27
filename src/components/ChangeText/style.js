3f62ddf86275f711bf0eb1ce7e4ca1bf9fc36767
Bubumariia Kudaiberdieva <mariiakudaiberdieva@gmail.com>
2022-09-27 15:18:50 +0300

    functional and class components

---
+++ src/components/ChangeText/style.js
@@ -0,0 +1,6 @@
+export const styleBtn = {
+    backgroundColor:'red',
+    padding: '1 rem'
+}
+
+
---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+import Logo from "./Logo"
+import ChangeText from "./ChangeText/ChangeText"
+
+function Header(props){
+    const { name,age } = props
+    return (
+        <React.Fragment>
+            <h1>Header {name} {age}</h1>
+            <Logo/>
+            <ChangeText/>
+        </React.Fragment>
+        
+    )
+}
+
+export default Header
